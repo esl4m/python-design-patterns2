297e1e4d844e5b798675fe72fa04dace33b4f430
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-12-05 20:31:48 +0200

    Adds facade pattern main

---
+++ 04_Facade_Pattern/main.py
@@ -0,0 +1,68 @@
+import time
+
+
+class TC1:
+    def run(self):
+        print("###### In Test 1 ######")
+        time.sleep(1)
+        print("Setting up")
+
+        time.sleep(1)
+        print("Running test")
+
+        time.sleep(1)
+        print("Tearing down")
+
+        time.sleep(1)
+        print("Test Finished\n")
+
+
+class TC2:
+    def run(self):
+        print("###### In Test 2 ######")
+        time.sleep(1)
+        print("Setting up")
+
+        time.sleep(1)
+        print("Running test")
+
+        time.sleep(1)
+        print("Tearing down")
+
+        time.sleep(1)
+        print("Test Finished\n")
+
+
+class TC3:
+    def run(self):
+        print("###### In Test 3 ######")
+        time.sleep(1)
+        print("Setting up")
+
+        time.sleep(1)
+        print("Running test")
+
+        time.sleep(1)
+        print("Tearing down")
+
+        time.sleep(1)
+        print("Test Finished\n")
+
+
+# Facade
+class TestRunner:
+    def __init__(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+
+    def runAll(self):
+        self.tc1.run()
+        self.tc2.run()
+        self.tc3.run()
+
+
+# Client
+if __name__ == '__main__':
+    testrunner = TestRunner()
+    testrunner.runAll()
