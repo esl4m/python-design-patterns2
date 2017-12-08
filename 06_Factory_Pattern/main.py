3d995526cbec9e3d2b7175ba99fe2044f3572597
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-12-08 03:28:24 +0200

    Adds pattern 06 - Factory Pattern

---
+++ 06_Factory_Pattern/main.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self):
+        self.name = None
+        self.gender = None
+
+    def getName(self):
+        return self.name
+
+    def getGender(self):
+        return self.gender
+
