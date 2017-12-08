bada884b143ac2be296108b25521fd31316bc1bc
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-12-08 16:51:01 +0200

    Adds Factory Pattern

--- 06_Factory_Pattern/main.py
+++ 06_Factory_Pattern/main.py
@@ -9,3 +9,25 @@ class Person:
     def getGender(self):
         return self.gender
 
+
+class Male(Person):
+    def __init__(self, name):
+        print("Hello Mr." + name)
+
+
+class Female(Person):
+    def __init__(self, name):
+        print("Hello Miss." + name)
+
+
+class Factory:
+    def getPerson(self, name, gender):
+        if gender == 'M':
+            return Male(name)
+        if gender == 'F':
+            return Female(name)
+
+
+if __name__ == '__main__':
+    factory = Factory()
+    person = factory.getPerson("Chetan", "M")
