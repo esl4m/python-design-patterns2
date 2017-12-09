f63899ffde8c18f6cf536ab8da3cdabfd1b2c33b
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-12-09 19:21:44 +0200

    Adds proxi pattern

---
+++ 07_Proxi_Pattern/main.py
@@ -0,0 +1,39 @@
+import time
+
+
+class Manager(object):
+    def work(self):
+        pass
+
+    def talk(self):
+        pass
+
+
+class SalesManager(Manager):
+    def work(self):
+        print("Sales Manager working...")
+
+    def talk(self):
+        print("Sales Manager ready to talk")
+
+
+class Proxy(Manager):
+    def __init__(self):
+        self.busy = 'No'
+        self.sales = None
+
+    def work(self):
+        print("Proxy checking for Sales Manager availability")
+
+        if self.busy == 'Yes':
+            self.sales = SalesManager()
+            time.sleep(2);
+            self.sales.talk()
+        else:
+            time.sleep(2);
+        print("Sales Manager is busy")
+
+
+if '__name__' == '__main__':
+    p = Proxy()
+    p.work()
